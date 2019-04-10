52882c904746e000cd9eaa50b7d93603ecd2c4db
phloempankaj <phloem_pankaj@hotmail.com>
2019-04-10 22:18:35 +0530

    displaydata-final

---
+++ displaydata/src/app/Employee.ts
@@ -0,0 +1,7 @@
+export default class Employee {
+    id: number;
+    name: string;
+    gender: string;
+    city: string;
+    salary: number;
+}
--- displaydata/src/app/app.component.ts
+++ displaydata/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Employee from './Employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'displaydata';
+  title = 'Displaying Data with Angular';
+  employees : Employee[] = [
+    { id: 1 , name: 'pankaj', gender: 'male', city: 'delhi', salary: 14000},
+    { id: 2 , name: 'priya', gender: 'female', city: 'pune', salary: 18000},
+    { id: 3 , name: 'hemant', gender: 'male', city: 'kolkata', salary: 24000},
+    { id: 4 , name: 'prachi', gender: 'female', city: 'noida', salary: 21000},
+    { id: 5 , name: 'pankaj', gender: 'male', city: 'noida', salary: 24500},
+  ];
 }
